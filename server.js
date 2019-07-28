e811b02e8c76bf94844f88093f836f5ca125737e
Tim Hatch <hatch.tim@gmail.com>
2019-07-28 13:10:20 +0100

    Call application code from server

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-// const express = require('express')
-import express from 'express'
+import express      from 'express'
+
+import Store        from './src/store'
+import HTTPService  from './src/services/egw-accessor'
+import { reducer }  from './src/services/results__list-c'
+
+const strip = (r) => {
+  delete r.acl
+  delete r.federation
+  delete r.fed_url
+  delete r.fed_id
+  delete r.rank_value
+  delete r.result_modified
+  delete r.result_detail1
+  delete r.result_detail5
+  delete r.result_detail6
+  delete r.result_detail7
+  delete r.rkey
+  delete r.url
+  return r
+}
+
+async function fetch(store, response) {
+  let results = await store.fetchResults()
+  let data    = reducer(store)().map(strip)
+  response.send(JSON.stringify(data))
+}
+
+const store = new Store(HTTPService)
+
+// BASIC EXPRESS SERVER IMPLEMENTATION
 
 const app  = express()
 const port = process.env.PORT || 3000
 
 app.get('/results', (req, res) => {
-  let params = { comp: 7844, cat: 42, ...req.query }
-  res.send('params: ' + JSON.stringify(params))
+  store.setRequestParams(req.query)
+  fetch(store, res)
 })
 
 app.use(function (req, res, next) {
@@ -18,5 +47,5 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Error 500')
 })
 
-app.listen(port, () => console.log('listening on http://localhost:' + port))
+app.listen(port, () => console.log('listening on:' + port))
 
